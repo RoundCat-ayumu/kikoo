0e226ee522b7eb33f8603d32929b05a21d960d18
AyumuNew <ayumuNew@RoundCat.local>
2021-01-31 10:30:15 -0800

    second commit

---
+++ user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from . import views
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path('home/', views.HomeView.as_view(), name='home')
+]
